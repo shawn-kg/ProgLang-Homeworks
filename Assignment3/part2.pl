8326461a7e98d344e880d7928423e3e9331d171e
cshen7099 <cshen7099@gmail.com>
2021-11-22 15:39:07 -0500

    Co-authored-by: shawn-kg <shawn-kg@users.noreply.github.com>

--- Assignment3/part2.pl
+++ Assignment3/part2.pl
@@ -8,17 +8,18 @@ read_loop :-
 	length(Line, Len),
 	(
 		Len > 0,
-		% write(Line), nl,
-		definition(I,C,A,D,Line,[]),
-		write(I), nl,
-		write(C), nl,
-		write(A), nl,
-		write(D), nl,
+		write(Line), nl,
+		% % definition(I,C,A,D,Line,[]),
+		% write(I), nl,
+		% write(C), nl,
+		% write(A), nl,
+		% write(D), nl,
 		read_loop;
 		Len =< 0,
 		write('Read last line.'), nl
 	).
-	
+
+
 definition(I,C,A,D) -->  two_words, id(I), one_word, core(C), three_words, area(A), five_words, dollars(D), two_words.
 % instead of id(I) do : char_type(I, alpha), id(I)
 one_word --> word.
@@ -32,6 +33,34 @@ area(4) --> [4].
 dollars(1000) --> [1000].
 
 word --> [processor] | [type] | [has] | [cores] | [','] | [square] | [centimeters] | [and] | [costs] | [dollars] | ['.'] | [uses].
+
+constraint(Attr,Comp) --> attribute(Attr), imperative, [be], comparison(Comp), value.
+constraint(Attr,RangeMin,RangeMax) --> attribute(Attr), imperative, [be], interval, range(RangeMin,RangeMax).
+
+attribute(Attr) --> attr_first, attr_second(Attr), attr_third.
+attr_first --> [the] | [].
+attr_second(core) --> [core].
+attr_second(area) --> [area].
+attr_second(cost) --> [cost].
+attr_third --> [count] | [].
+
+imperative --> imp_first, imp_second.
+imp_first --> [must] | [is].
+imp_second --> [to] | [].
+
+comparison(Comp) --> comp_first(Comp), comp_second.
+comp_first(equal) --> [equal].
+comp_first(less) --> [less].
+comp_first(greater) --> [greater].
+comp_second --> [to] | [than].
+
+value --> integer. % how do i do this
+interval --> [in], [the], intv_choices, [of].
+intv_choices --> [interval] | [range].
+
+range(RangeMin,RangeMax) --> range_min(RangeMin), [to], range_max(RangeMax). % closed aka inclusive
+range_min --> integer.
+range_max --> integer.
 
 % constraint(Attr, Imp, X, Y) --> attribute(Attr), imperative(Imp), one_word, constr(X).	
 
