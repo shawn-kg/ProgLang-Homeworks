47c29b5dac73f876868d252d25aeb846933de043
Shawn <georgs2@rpi.edu>
2021-11-24 18:48:09 -0500

    Cleaning stuff up

--- Assignment3/part2.pl
+++ Assignment3/part2.pl
@@ -26,9 +26,6 @@ read_loop(ProcList,ConstList) :-
 
 
 definition(I,C,A,D) -->  two_words, id(I), one_word, core(C), three_words, area(A), five_words, dollars(D), two_words.
-% definition --> [processor,type,,has,,cores,',',uses,,square,centimeters,,,and,costs,,dollars,'.'].
-% instead of id(I) do : char_type(I, alpha), id(I)
-% definition([processor, type, [abc], has, [16], cores, ',', uses, [2], square, centimeters, and, costs, [5], dollars, '.'], [processor, type, has,cores, ',', uses, square, centimeters, and, costs, dollars, '.']).
 
 one_word --> word.
 two_words --> word, word.
@@ -93,67 +90,8 @@ value(V) -->  [V], { integer(V) }.
 interval --> [in], [the], intv_choices, [of].
 intv_choices --> [interval] | [range].
 
-% range(RangeMin,RangeMax) --> range_min(RangeMin), [to], range_max(RangeMax). % closed aka inclusive
 range_min(RangeMin) --> [RangeMin], { integer(RangeMin) }.
 range_max(RangeMax) --> [RangeMax], { integer(RangeMax) }.
-
-% constraint(Attr, Imp, X, Y) --> attribute(Attr), imperative(Imp), one_word, constr(X).	
-
-% attribute(W) --> att_word(W).
-% attribute(W, V) --> att_word(W), att_word(V).
-% attribute(W, V, X) --> att_word(W), att_word(V), att_word(X).
-
-% imperative(W) --> imp_word(W).
-% imperative(W,V) --> imp_word(W), imp_word(V).
-
-% comparison(W,V) --> comp_word(W), comp_word(V).
-
-% att_word(the) --> [the]. 
-% att_word(core) --> [core].
-% att_word(count) --> [count].
-% att_word(area) --> [area].
-% att_word(cost) --> [cost].
-
-% imp_word(must) --> [must].
-% imp_word(is) --> [is].
-% imp_word(to) --> [to].
-
-% comp_word(equal) --> [equal].
-% comp_word(less) --> [less].
-% comp_word(greater) --> [greater].
-% comp_word(than) --> [than].
-% comp_word(to) --> [to].
-
-% include(Goal, List, Included) :-
-% 	phrase(include_(List, Goal), Included).
-
-% include_([], _) --> [].
-% include_([L|Ls], Goal) -->
-% 	(   { call(Goal, L) } ->
-% 		[L]
-% 	;   []
-% 	),
-% 	include_(Ls, Goal).
-
-%%%%%%%%
-
-
-% integer(I) -->
-% 	digit(D0),
-% 	digits(D),
-% 	{ number_codes(I, [D0|D])
-% 	}.
-
-% digits([D|T]) -->
-% 	digit(D), !,
-% 	digits(T).
-% digits([]) -->
-% 	[].
-
-% digit(D) -->
-% 	[D],
-% 	{ code_type(D, digit)
-% 	}.
 
 
 
