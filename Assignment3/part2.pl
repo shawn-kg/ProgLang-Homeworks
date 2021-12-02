b46cf13e6e4205e9b29941b8cb4477948331d194
cshen7099 <cshen7099@gmail.com>
2021-12-02 15:28:51 -0500

    commented parts 1 and 2

--- Assignment3/part2.pl
+++ Assignment3/part2.pl
@@ -1,9 +1,18 @@
+%-------------------------------------------------------
+% Programming Languages CSCI 4430
+% Programming Assignment 3 Part 2
+% Shawn George (georgs2) and Chrisanna Shen (shenc6)
+%-------------------------------------------------------
+
+% libraries used
 :- use_module(library(clpfd)).
 :- use_module(library(dcg/basics)).
 :- use_module(library(lists)).
 
 :- [read_line].
 
+% reads the input line by line, parsing with definition/6 and constraint/5 and appending them to lists
+% all the possible processor combinations that satisfy the input constrain sentences are identified 
 read_loop(ProcList,ConstList) :-
 	read_line(Line),
 	length(Line, Len),
@@ -26,7 +35,7 @@ read_loop(ProcList,ConstList) :-
 
 
 
-
+% processor definition sentence syntax
 definition(I,C,A,D) -->  two_words, id(I), one_word, core(C), three_words, area(A), five_words, dollars(D), two_words.
 
 one_word --> word.
@@ -34,17 +43,20 @@ two_words --> word, word.
 three_words --> word, word, word.
 five_words --> word, word, word, word, word.
 
-
+% checking and binding relevant values (id, core count, area, and cost) to variables.
 id(I) --> [I], {is_alpha(I)}. 
 core(C) --> [C], {integer(C)}.
 area(A) --> [A], {integer(A)}.
 dollars(D) --> [D], {integer(D)}.
 
+% acceptable words used in definition sentences
 word --> [processor] | [type] | [has] | [cores] | [','] | [square] | [centimeters] | [and] | [costs] | [dollars] | ['.'] | [uses].
 
+% constraint sentence definition syntax
 constraint(Attr,Comp,V) --> attribute(Attr), imperative, [be], comparison(Comp), value(V), ['.'].
 constraint(Attr,RangeMin,RangeMax) --> attribute(Attr), imperative, [be], interval, range_min(RangeMin), [to], range_max(RangeMax), ['.'].
 
+% constraint attribute definition
 attribute(Attr) --> attr_first, attr_second(Attr), attr_third.
 attr_first --> [the] | [].
 attr_second(core) --> [core].
@@ -52,26 +64,35 @@ attr_second(area) --> [area].
 attr_second(cost) --> [cost].
 attr_third --> [count] | [].
 
+% constraint imperative definition
 imperative --> imp_first, imp_second.
 imp_first --> [must] | [is].
 imp_second --> [to] | [].
 
+% constraint comparison term definition
 comparison(Comp) --> comp_first(Comp), comp_second.
 comp_first(equal) --> [equal].
 comp_first(less) --> [less].
 comp_first(greater) --> [greater].
 comp_second --> [to] | [than].
 
+% binding constraint value to variable
 value(V) -->  [V], { integer(V) }.
 
+% constraint interval phrase definition
 interval --> [in], [the], intv_choices, [of].
 intv_choices --> [interval] | [range].
 
+% constrain interval values binded to variables
 range_min(RangeMin) --> [RangeMin], { integer(RangeMin) }.
 range_max(RangeMax) --> [RangeMax], { integer(RangeMax) }.
 
 
+% Evaluates constraint, depending on if constraint attribute is core, area, or cost. 
 
+% Possible values are between 0 to 16 processors (inclusive), so a list with length of the number 
+% of processor definitions will contain this domain as each element. This list of 0..16 is passed to
+% helper functions to check if the combination of number of corresponding processors satisfy the constraints.
 constraints(_,[],_).
 constraints(ProcList,[[Attr, X, Y]|T],Processors) :-
 	length(ProcList, L), length(Processors, L), Processors ins 0..16, 
@@ -105,6 +126,7 @@ constraints(ProcList,[[Attr, X, Y]|T],Processors) :-
 	Costsum #=< Y)), 
 	constraints(ProcList,T,Processors))).
 
+% the number of each processor and their ids are passed to output/4 to print out
 answers(ProcList, [Answers | Tail]):-
 	getIds(ProcList, Ids, 0),
 	length(Ids, Anslen),
@@ -112,6 +134,7 @@ answers(ProcList, [Answers | Tail]):-
 	answers(ProcList, Tail).
 answers(_,[]).
 
+% writes solution to terminal
 output([],[],_, _) :-
 	write(';'), nl.
 output([H1|T1],[H2|T2],L, Counter) :-
@@ -120,31 +143,35 @@ output([H1|T1],[H2|T2],L, Counter) :-
 	(checkCounter(NewCounter, L) ; true),
 	output(T1, T2, L, NewCounter).
 
+% helper function to check when to add comma (as opposed to a semicolon)
 checkCounter(Counter, L) :-
 	(Counter<L -> write(', ')).
 
+% stores the processors' id in a list
 getIds([],_,_).
 getIds([H|T], [NHH|NHT], Ind) :-
 	nth0(Ind,H,X),
 	NHH = X, 
 	getIds(T,NHT,Ind).
 
-
+% helper function used in checking constraints
+% used to get a list of constraint values corresponding to the index passed in
 getValues([],_,_).
 getValues([H|T], [NHH|NHT], Ind) :-
 	nth0(Ind,H,X),
 	NHH is X,
 	getValues(T,NHT,Ind).
 
-% Do this getValues(ProcList, AttrConst, Index).
-% NewList is a list of Attribute constraint constants
-	
+% helper function used in checking constraints
+% used to get the sum of a certain combination of constraint property values
+% this is later compared with the input constraints to check satisfaction
 multList([],[],0).
 multList([H1|T1], [H2|T2], Value) :-
 	Temp = H1*H2,
 	multList(T1,T2, Rest),
 	Value = Temp + Rest.
 
+% main
 main :-
 	read_loop([], []).
 
--- Assignment3/relations.pl
+++ Assignment3/relations.pl
@@ -1,24 +1,38 @@
+%-------------------------------------------------------
+% Programming Languages CSCI 4430
+% Programming Assignment 3 Part 1
+% Shawn George (georgs2) and Chrisanna Shen (shenc6)
+%-------------------------------------------------------
+
+% Person and Sibling are children of the same parent
+% Person and Sibling cannot be the same person
 siblings(Person, Sibling) :-
 	parentOf(X,Person), parentOf(Y,Sibling), X=Y, Person\=Sibling.
 
+% one of Person's parents is siblings with one of Cousin's parents
 firstCousins(Person, Cousin) :-
 	parentOf(X,Person), parentOf(Y,Cousin), siblings(X,Y), Person\=Cousin.
 
+% Person is either a child, grandchild, great-grandchild, et cetera, of Ancestor
 hasAncestor(Person, Ancestor) :-
 	parentOf(Ancestor, Person).
 hasAncestor(Person, Ancestor) :-
     parentOf(X, Person), hasAncestor(X, Ancestor).
 
+% Person is either a parent, grandparent, great-grandparent, et cetera, of Descendant 
+% Person is an ancestor of Descendant
 hasDescendant(Person, Descendant) :-
 	hasAncestor(Descendant, Person).
 
+% using findall to get the list of all of the people with the ancestor Ancestor
 listAncestors(Person, Ancestors) :-
 	findall(Ancestor, hasAncestor(Person,Ancestor), Ancestors).
 
-
+% useing findall to get the list of all the descendants of Person
 listDescendants(Person, Descendants) :-
 	findall(Descendant, hasDescendant(Person,Descendant), Descendants).
 
+% Person must be monarch, and oldest checks if Heir is the Person's oldest child.
 hasHeir(Person, Heir) :-
 	monarch(Person), 
 	findall(Child, parentOf(Person, Child), Children), 
@@ -26,16 +40,19 @@ hasHeir(Person, Heir) :-
 	birthYear(Heir, HeirYear),
 	oldest(Person, Children, Heir, HeirYear).
 
+% oldest/4 checks if the birth year of the person passed in is the earliest of 
+% the parents' children, making them the oldest.
 oldest(_, [], _, _).
 oldest(Parent, [Sibling|Rest], Child, Year):-
 	birthYear(Sibling, SibYear), SibYear >= Year, oldest(Parent, Rest, Child, Year).
 
-
+% if Person was a monarch, their successor is any of their children who also were/are a monarch.
 hasSuccessor(Person, Successor) :-
 	childOf(Successor, Person),
 	monarch(Person),
 	monarch(Successor).
 
+% Person's original heir ended up succeeding them
 heirIsSuccessor(Person) :-
 	hasHeir(Person, Heir),
 	hasSuccessor(Person, Successor),
